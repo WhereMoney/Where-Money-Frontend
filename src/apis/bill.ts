4aeb4b5e8ae966aedd6124d85606cf3f20758e2d
殊怀丶 <lvzhihe_123@qq.com>
2022-05-29 00:50:28 +0800

    feat: 完成首页

--- src/apis/bill.ts
+++ src/apis/bill.ts
@@ -1,7 +1,27 @@
 import http from "@/utils/http";
+import { NumberAnimationInst } from "naive-ui";
 
 function addBillApi(params: any): any {
     return http.post("/bill/add-bill", params);
 }
 
-export { addBillApi };
+// 获取指定账本的所有账单
+function getAllBillInBookApi(params:{bookId:number}):any{
+    return http.get('/bill/all-bill',params)
+}
+
+// 分日统计指定账本所有账单
+function getBillForTimeApi(params:any):any{
+    return http.get('/bill/day-statistic-time',params)
+}
+
+// 获取指定账本下的所有账单分类
+function getBillCategoryApi(params:{bookId:number}):any{
+    return http.get('/bill/get-bill-category',params)
+}
+
+//分类统计指定账本指定日期内的账单
+function getBillInStatisticTimeApi(params:{bookId:NumberAnimationInst,startTime:string,endTime:string}):any{
+    return http.get('/bill/category-statistic-time',params)
+}
+export { addBillApi,getAllBillInBookApi,getBillForTimeApi,getBillCategoryApi,getBillInStatisticTimeApi};
